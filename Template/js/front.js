19e30b1986b00eee9ab4ee57988eab82894bf4b5
Mark <markjcorum@gmail.com>
2020-01-21 08:44:02 -0500

    Console Error
    Fixed console errors... among other things probably

--- Template/js/front.js
+++ Template/js/front.js
@@ -7,28 +7,6 @@ $(document).ready(function () {
     // For demo purposes only
     // ------------------------------------------------------ //
 
-    var stylesheet = $('link#theme-stylesheet');
-    $( "<link id='new-stylesheet' rel='stylesheet'>" ).insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
-
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
-
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
-
-        }
-
-        return false;
-    });
 
 
     // ------------------------------------------------------- //
@@ -60,7 +38,7 @@ $(document).ready(function () {
     // ---------------------------------------------- //
     // FancyBox
     // ---------------------------------------------- //
-    $("[data-fancybox]").fancybox();
+    //$("[data-fancybox]").fancybox();
 
 
     // ---------------------------------------------- //
