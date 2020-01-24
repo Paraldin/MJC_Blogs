66d6cf0ebe338eb915c3c7e19f38d3eb6154d9c5
Mark <markjcorum@gmail.com>
2020-01-24 08:55:32 -0500

    Revert "LinkedIn works"
    This reverts commit 2bdf2d3148e861e131120b2ed38ae8c840867831.

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -55,11 +55,12 @@ namespace MJC_Blogs
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            app.UseFacebookAuthentication(
-               appId: "2565349196921685",
-               appSecret: "a278fc9430877c50bca8f9562dd4ce7b");
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
 
-
+<<<<<<< HEAD
+<<<<<<< HEAD
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
                 ClientId = "604318586118-thegl6uv1gmmue96pq23q0jv7vm4jn2m.apps.googleusercontent.com",
@@ -69,6 +70,18 @@ namespace MJC_Blogs
             app.UseLinkedInAuthentication(
                 clientId: "78ry1x7eud1xqh", 
                 clientSecret: "JqTojFE8dJ8mlpl1");
+=======
+=======
+>>>>>>> parent of e7893b2... Login G, F
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+<<<<<<< HEAD
+>>>>>>> parent of e7893b2... Login G, F
+=======
+>>>>>>> parent of e7893b2... Login G, F
         }
     }
 }
